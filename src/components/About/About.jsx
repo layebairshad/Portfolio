a76c7f3ac4da5438f95177df0e487987bc8d82af
Layeba Irshad <127973007+layebairshad@users.noreply.github.com>
2025-09-06 12:17:36 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1jpdk_VONt63NpUVSvONCHP6GzyJcuXCi/view?usp=sharing"
+            href="https://drive.google.com/file/d/1mWzIpTxbJXZPWJYlHuSgDr3KTZAd-_As/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -60,7 +60,7 @@ const About = () => {
               boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
             }}
           >
-            DOWNLOAD CV
+            RESUME
           </a>
         </div>
         {/* Right Side */}
